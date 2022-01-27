fff055f0a434af419b54628fb9ff8e588a8ca26e
Red_M <git@red-m.net>
2022-01-27 12:28:06 +1000

    Add stepper selection config option.

--- stable_z_home.py
+++ stable_z_home.py
@@ -10,27 +10,35 @@ import logging
 
 
 class StableZHome:
+
+    cmd_STABLE_Z_HOME_help = "Repeatedly home Z until the Z stepper position stabilizes"
+
     def __init__(self, config):
         self.printer = config.get_printer()
         self.gcode = self.printer.lookup_object('gcode')
         gcode_macro = self.printer.load_object(config, 'gcode_macro')
         self.before_homing_gcode = gcode_macro.load_template(config, 'gcode')
         self.default_max_retries = config.getint("retries", 20, minval=0)
-        self.default_retry_tolerance = \
-            config.getfloat("retry_tolerance", 1 / 400., above=1/1000.)
+        self.default_retry_tolerance = config.getfloat("retry_tolerance", 1 / 400., above=1/1000.)
         self.default_window = config.getint("window", 4, minval=3)
+        self.stepper_enable = self.printer.load_object(config, 'stepper_enable')
+        self.stepper_selection = config.getchoice
         # Register STABLE_Z_HOME command
-        self.gcode.register_command(
-            'STABLE_Z_HOME', self.cmd_STABLE_Z_HOME,
-            desc=self.cmd_STABLE_Z_HOME_help)
-    cmd_STABLE_Z_HOME_help = (
-        "Repeatedly home Z until the Z stepper position stabilizes")
+        self.gcode.register_command('STABLE_Z_HOME', self.cmd_STABLE_Z_HOME, desc=self.cmd_STABLE_Z_HOME_help)
+        self.printer.register_event_handler("klippy:connect", self.handle_connect)
+
+    def handle_connect(self):
+        # Stepper lookup
+        all_steppers = self.stepper_enable.get_steppers()
+        steppers_selection = {}
+        for stepper_name in all_steppers:
+            if stepper_name.startswith('stepper_z'):
+                steppers_selection[stepper_name] = stepper_name
+        self.homing_stepper = self.stepper_selection('homing_stepper', steppers_selection, 'stepper_z')
+
     def cmd_STABLE_Z_HOME(self, gcmd):
-        max_retries = gcmd.get_int('RETRIES', self.default_max_retries,
-                                   minval=0)
-        retry_tolerance = gcmd.get_float('RETRY_TOLERANCE',
-                                         self.default_retry_tolerance,
-                                         minval=1/1000.)
+        max_retries = gcmd.get_int('RETRIES', self.default_max_retries, minval=0)
+        retry_tolerance = gcmd.get_float('RETRY_TOLERANCE', self.default_retry_tolerance, minval=1/1000.)
         window = gcmd.get_int('WINDOW', self.default_window, minval=3)
 
         toolhead = self.printer.lookup_object('toolhead', None)
@@ -47,23 +55,20 @@ class StableZHome:
         steppers = kin.get_steppers()
         stepper = None
         for s in steppers:
-            if s.get_name().startswith('stepper_z'):
+            if s.get_name()==self.homing_stepper:
                 stepper = s
                 break
         if stepper is None:
             raise gcmd.error("No Z steppers found")
 
-        self.gcode.respond_info(
-            'Stable Z home: %.4f tolerance, window %d, %d max retries\n'
-            % (retry_tolerance, window, max_retries))
+        self.gcode.respond_info('Stable Z home: %.4f tolerance, window %d, %d max retries\n' % (retry_tolerance, window, max_retries))
 
         mcu_z_readings = []
         retries = 1
         retry_tolerance += 1e-4  # allow for floating point rounding errors
         while retries <= max_retries:
             try:
-                self.gcode.run_script_from_command(
-                    self.before_homing_gcode.render())
+                self.gcode.run_script_from_command(self.before_homing_gcode.render())
             except Exception:
                 logging.exception("Exception running pre-home script")
                 raise self.gcode.error('Pre-home Gcode failed')
@@ -79,11 +84,8 @@ class StableZHome:
             else:
                 window_range = None
 
-            window_range_str = \
-                '%.4f' % (window_range,) if window_range is not None else '-'
-            self.gcode.respond_info(
-                'Retry %d: %s position %.4f, window range %s\n'
-                % (retries, stepper.get_name(), mcu_pos, window_range_str))
+            window_range_str = '%.4f' % (window_range,) if window_range is not None else '-'
+            self.gcode.respond_info('Retry %d: %s position %.4f, window range %s\n' % (retries, stepper.get_name(), mcu_pos, window_range_str))
 
             if window_range is not None and window_range <= retry_tolerance:
                 self.gcode.respond_info('Succeeded\n')
